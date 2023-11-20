b5d00537acc7e85f5ff707bb886f10b157c05dd7
amayaling <123601690+amayaling@users.noreply.github.com>
2023-11-19 23:19:55 -0800

    done

--- Elevators.java
+++ Elevators.java
@@ -4,82 +4,75 @@ public class Elevators{
 
 	Floors f = new Floors(); 
 	Passengers pass = new Passengers(); 
-// priority queue sorts in place basically 
-	Queue<Integer> up = new PriorityQueue<>();
-	Queue<Integer> down = new PriorityQueue<>();
 
-	List<Integer> timeList = new ArrayList<>(); // to track time in duration 
+	List<Integer> timeList;  // to track time in duration 
+	
 
+	boolean moveUp = true; // moving up 
 
-	//List<Passenger> passenger; 
-	//List<Floors> floors;  
+	
+	int currFloor = 1; // start floor 
+	int topFloor;  // gets very last floor 
+	int capacity; 
 
-	boolean elevatorDirection = true; // moving up 
+	String dataStructure;
 
-	int capacity = f.getCap(); 
-	int floorNum = f.getCurrFloor(); 
-	int topFloor = f.getNumFloors(); // gets very last floor 
-
-
-	int passengers = f.generatePassenger(); 
 
 	int numElevators = 1; // at least one elevator, but that is subject to change 
 
 	int duration;
 
+
 	public void setDuration(int d){
 		duration = d; 
 	}
-
-	public void setElevators(int elevators){
-		numElevators = elevators; 
+// getting the very top floor
+	public void setTopFloor(int fl){
+		topFloor = fl;
 	}
 
-	public boolean move(){
-		if (elevatorDirection){
-			floorNum++; 
-			return true;
-			// if direction is up, its true
-		}else{
-			floorNum--; 
-			return false; 
-		}
+	public int getTopFloor(){
+		return topFloor;
 	}
 
-// loading in passengers
-	// was previosuly in floor function but i needed to move it 
-	public void load(){
-		Random r = new Random(); 
-		if (passengers > 0 && passengers <= capacity){
-			int destination = r.nextInt(topFloor) + 1; // getting a number within the range of all the floors 
-			if (destination > floorNum && destination <= topFloor){
-				up.add(destination); 
-			}
-			else{
-				down.add(destination);
-			}
-			elevatorDirection = true; 
-			
+	public void setCap(int cap){
+		capacity = cap;
+	}
 
-		}
-		else if (passengers <= 0){
-			f.generatePassenger();
-			
+	public void setDS(String structure){
+		// changes the data structure 
+		if (structure.contains("array")){
+			timeList = new ArrayList<>(); 
 		}
 		else{
-			elevatorDirection = false; // quit moving up, time to move down
-			
+			timeList = new LinkedList<>(); 
+		}
+	}
+	
+// couldnt figure out the multiple elevators 
+	 public void setElevators(int elevators){
+	 	numElevators = elevators; 
+	 }
 
+// moves the elevator up or down
+	public void move(){
+		if (moveUp){
+			currFloor++; 
+			// if direction is up, its true
+		}else if (!moveUp) {
+			currFloor--; 
+			
 		}
 	}
 
 	public void unload(boolean direction, int floorNum){
-		while ((up.isEmpty() == false) && (down.isEmpty() == false)){
-			if (floorNum == floorNum && direction == true){
-				up.remove(floorNum); 
+		while ((f.upQueue().isEmpty() == false) && (f.downQueue().isEmpty() == false)){
+			if (currFloor == floorNum && f.load() == true){
+				f.upQueue().remove(floorNum); 
 			}
-			else if (floorNum == floorNum && direction == false) {
-				down.remove(floorNum); 
+			else if (currFloor == floorNum && f.load() == false) {
+				f.downQueue().remove(floorNum); 
+
 
 			}
 		}
@@ -88,26 +81,59 @@ public class Elevators{
 // button is true, create elevators
 	// really need to fix elevator sim and the logic here 
 	// need to make sure the 
-	public void elevatorSim(boolean button){
-		if (button){
-			System.out.println(topFloor);
-			System.out.println(floorNum);
-			System.out.println(passengers);
-			System.out.println(duration);
-			for (int i = 0; i < duration; i++){
-				f.generatePassenger(); // getting a passenger
-				load();
-				elevatorDirection = true; 
-				move(); 
-				if (i == f.getNumFloors()){
-					elevatorDirection = false;
-					move(); // turning that thing around
-					unload(elevatorDirection, floorNum); 
+	public void elevatorSim() {
+    for (int i = 0; i < duration; i++) {
+        f.generatePassenger();
+        f.load();
+        timeList.add(duration);
+        if (moveUp) {
+            move();
+            if (currFloor == topFloor) {
+                moveUp = false;
+                unload(moveUp, currFloor);
+                timeList.add(i);
+            }
+        } else if (!f.load()){
+        	moveUp = false; 
+        	move();
+        }
+        else {
+            move();
+            if (currFloor == 1) {
+                moveUp = true;
+                unload(moveUp, currFloor);
+                timeList.add(i);
+            }
+        }
+    }
+}
 
-				}
+
+	public void printResults(){
+		// prints results of the elevator simulation 
+		int maxTemp = timeList.get(0);
+		for (int k = 1 ; k < timeList.size(); k++){
+			if (timeList.get(k) > maxTemp){
+				maxTemp = timeList.get(k);
 			}
-
 		}
+		System.out.println("Longest time: " + maxTemp); 
+
+		int minTemp = timeList.get(0);
+		for (int j = 1; j < timeList.size(); j++){
+			if (timeList.get(j) < minTemp){
+				minTemp = timeList.get(j);
+			}
+		}
+		System.out.println("Shortest time: " + minTemp); 
+
+		int sum = 0;
+		for (int h = 0; h < timeList.size(); h++){
+			sum += timeList.get(h);
+		}
+		System.out.println("Average time: " + sum/timeList.size());
+
+		
 	}
 
 }
